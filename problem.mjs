17129d7a49ff3f49dc15558b14782da4685f1bec
neecholaus <thisisnickadam@gmail.com>
2020-11-05 19:28:32 -0800

    more setup

--- problem.mjs
+++ problem.mjs
@@ -2,12 +2,10 @@
 // 2. have maximum weight limit
 // 3. maximize the item.value sum while remaining under weight limit
 
-const items = [
+export const items = [
     {id: 1, val: 10, w: 3},
     {id: 2, val: 6, w: 8},
     {id: 3, val: 3, w:3}
 ];
 
-const maxWeight = 8;
-
-
+export const maxWeight = 8;
