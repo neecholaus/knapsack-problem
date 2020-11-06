1e9168d19848b664ecd08aaa3396f4cb3fb9aa38
neecholaus <thisisnickadam@gmail.com>
2020-11-05 19:48:32 -0800

    cleanup

--- solution.mjs
+++ solution.mjs
@@ -1,6 +1,17 @@
 import {items, maxWeight} from './problem';
 
-// sort by val desc and w asc
+// sort by val desc
+// no spaceship operator :(
+items.sort((a, b) => {
+    if (a.val > b.val)
+        return -1;
+
+    if (a.val < b.val)
+        return 1
+
+    if (a.val === b.val)
+        return 0;
+});
 
 // iterate through each from start, adding val and weight
     // once max weight is reached / exceeded
