545ffe4af980796cc992370edcd674709048de73
neecholaus <thisisnickadam@gmail.com>
2020-11-05 20:04:19 -0800

    base loop started, comparison loop begun skipping starting item

--- solution.mjs
+++ solution.mjs
@@ -1,4 +1,8 @@
 import {items, maxWeight} from './problem';
+const x = {id: 4, w: 5, v: 2};
+const y = {...x};
+delete y.id;
+console.log(y);
 
 // sort by val desc
 // no spaceship operator :(
@@ -13,10 +17,28 @@ items.sort((a, b) => {
         return 0;
 });
 
-// iterate through each from start, adding val and weight
-    // once max weight is reached / exceeded
+// holder for knapsack possibilities
+// array of {items: array, val: int, w: int}
+let theoreticalKnapsacks = [];
+
+// iterate through each item
+items.forEach(startingItem => {
+    // initialize current theory
+    let currentTheoreticalKnapsack = {...startingItem};
+    delete currentTheoreticalKnapsack.id;
+    currentTheoreticalKnapsack.items = [startingItem];
+
+    // iterate through every item adding val and weight
+    items.forEach(item => {
+        // skip our theories starting item that we did not start with,
+        if (item.id === startingItem.id)
+            return;
+
+        // once max weight is reached / exceeded
         // if max weight == accumulated weight
-            // since we started from highest vals, we have a winner, return list of itemsa
+        // since we started from highest vals, we have a winner, return list of itemsa
         // otherwise weight is higher than allowed
-            // remove current item
-            // store list of items as potential candidate, move on to next item
+        // remove current item
+        // store list of items as potential candidate, move on to next item
+    });
+});
