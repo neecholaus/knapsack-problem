fb06d25138fe51de34b4a13c4d47cdf9691f964c
neecholaus <thisisnickadam@gmail.com>
2020-11-05 19:21:06 -0800

    added initial problem

---
+++ solution.js
@@ -0,0 +1,13 @@
+// 1. have list of items, each item has weight and value
+// 2. have maximum weight limit
+// 3. maximize the item.value sum while remaining under weight limit
+
+const items = [
+    {id: 1, val: 10, w: 3},
+    {id: 2, val: 6, w: 8},
+    {id: 3, val: 3, w:3}
+];
+
+const maxWeight = 8;
+
+
